1c272b2d3c2086ecaf591bc508671f1f071ad8a8
javion89 <john_obsidian1@outlook.com>
2024-09-28 21:27:51 +0100

    update

--- main.tf
+++ main.tf
@@ -8,3 +8,17 @@ resource "aws_s3_bucket" "bucket" {
 
   bucket = "javionnews3bucket1"
  } 
+terraform {
+  backend "s3" {
+    bucket                  = "javionnews3bucket1"
+    key                     = "build/terraform.tfstate"
+    region                  = "eu-west-2"
+      }
+}
+resource "aws_instance" "test1" {
+  ami         = "ami-0fc7a46876a675c5f"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "window server"
+  }
+}
